4b4f6eb2e539892bf8c2f90d93895b83b90b7014
Viktor Djupsjöbacka <viktor.djupsjobacka@iki.fi>
2023-07-21 02:22:36 +0300

    Update production paths following switch to GitHub Pages deployment

--- config/index.js
+++ config/index.js
@@ -4,10 +4,10 @@ var path = require('path')
 module.exports = {
     build: {
         env: require('./prod.env'),
-        index: path.resolve(__dirname, '../dist/planets/index.html'),
-        assetsRoot: path.resolve(__dirname, '../dist/planets'),
+        index: path.resolve(__dirname, '../dist/index.html'),
+        assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/planets/',
+        assetsPublicPath: '/ProceduralPlanet/', // This matches the repository name, with the files deployed to pistolshrimpgames.github.io/ProceduralPlanet/
         productionSourceMap: true,
         // Gzip off by default as many popular static hosts such as
         // Surge or Netlify already gzip all static assets for you.
